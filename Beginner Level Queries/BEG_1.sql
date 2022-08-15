8f55f20bc2b4bb0d43fe6b4fecb24efdf5ba1c4f
jugal-chauhan04 <111266884+jugal-chauhan04@users.noreply.github.com>
2022-08-15 11:34:09 +0530

    Update BEG_1.sql

--- Beginner Level Queries/BEG_1.sql
+++ Beginner Level Queries/BEG_1.sql
@@ -1,6 +1,6 @@
 --In the OrderDetails table, we have the fields
 --UnitPrice and Quantity. Create a new field,
---TotalPrice, that multiplies these two together. We’ll
+--TotalPrice, that multiplies these two together. Weâ€™ll
 --ignore the Discount field for now.
 --In addition, show the OrderID, ProductID, UnitPrice,
 --and Quantity. Order by OrderID and ProductID.
@@ -16,3 +16,5 @@ FROM
 ORDER BY
 	OrderID,
 	ProductID
+	
+	
