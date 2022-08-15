dbb6a204779c871bd5c7c903b03efe99fd371caf
jugal-chauhan04 <111266884+jugal-chauhan04@users.noreply.github.com>
2022-08-15 11:32:25 +0530

    Add files via upload

---
+++ Beginner Level Queries/BEG_1.sql
@@ -0,0 +1,18 @@
+--In the OrderDetails table, we have the fields
+--UnitPrice and Quantity. Create a new field,
+--TotalPrice, that multiplies these two together. We’ll
+--ignore the Discount field for now.
+--In addition, show the OrderID, ProductID, UnitPrice,
+--and Quantity. Order by OrderID and ProductID.
+
+SELECT
+	OrderID,
+	ProductID,
+	UnitPrice,
+	Quantity,
+	(UnitPrice * Quantity) AS TotalPrice  --creating a new field TotalPrice using * operator
+FROM
+	Northwind.dbo.[Order Details]
+ORDER BY
+	OrderID,
+	ProductID
