4b31847075a8fab2d5de857a4fc3462807a52bed
jugal-chauhan04 <111266884+jugal-chauhan04@users.noreply.github.com>
2022-08-24 21:38:26 +0530

    Add files via upload

---
+++ Beginner Level Queries/BEG_8.sql
@@ -0,0 +1,23 @@
+--We’d like to show a list of the Orders that were
+--made, including the Shipper that was used. Show the
+--OrderID, OrderDate (date only), and CompanyName
+--of the Shipper, and sort by OrderID.
+--In order to not show all the orders (there’s more than
+--800), show only those rows with an OrderID of less
+--than 10300
+
+SELECT
+		OrderID,
+		CONVERT(Date, OrderDate) AS Date,  --using the DATE function to extract date only
+		CompanyName
+
+FROM
+	Northwind.dbo.Orders AS orders
+JOIN
+	Northwind.dbo.Shippers AS shipper
+ON 
+	orders.ShipVia = shipper.ShipperID
+WHERE
+	OrderID < 10300
+ORDER BY
+	OrderID
