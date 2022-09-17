654fbf2221d7024c115f74967d252f5b303674af
Jugal Chauhan <jugalchauhan404@gmail.com>
2022-09-17 16:57:31 +0530

    Add files via upload

---
+++ Advanced Level Queries/ADV_8.sql
@@ -0,0 +1,34 @@
+--Shwing the percentage of Late Orders over Total Orders.
+
+WITH AllOrders AS
+(SELECT
+	 EmployeeID, COUNT(*) AS TotalOrders
+FROM
+	Orders
+GROUP BY
+	EmployeeID
+),
+LateOrders AS
+(SELECT
+	 EmployeeID, COUNT(*) AS TotalOrders
+FROM
+	Orders
+WHERE
+	RequiredDate < ShippedDate
+GROUP BY
+	EmployeeID
+)
+	
+
+SELECT
+	Employees.EmployeeID,
+	Employees.LastName,
+	Allorders = AllOrders.TotalOrders,
+	Lateorders = LateOrders.TotalOrders,
+	PercentLateOrder = CAST(ROUND((LateOrders.TotalOrders*1.00/AllOrders.TotalOrders)*100, 2) as float)
+
+FROM Employees
+JOIN AllOrders
+ON Employees.EmployeeID = AllOrders.EmployeeID
+JOIN LateOrders
+ON Employees.EmployeeID = LateOrders.EmployeeID
